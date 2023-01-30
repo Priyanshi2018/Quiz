e9bc971e5911bb4d5ac693291112b345a0bc574b
Priyanshi2018 <varyanipriyanshi@gmail.com>
2023-01-30 17:18:43 +0530

    quiz app implemented toast

---
+++ src/QuizzApp/Quiz.js
@@ -0,0 +1,81 @@
+import React,{useState} from 'react';
+import './Quiz.css';
+import questions from "./QuizData";
+import QuizResult from './QuizResult';
+import {ToastContainer, toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+
+const Quizz = () => {
+    const [currentQuestion,setCurrentQuestion]=useState(0);
+    const [score,setScore]=useState(0);
+    const [CorrectAns,setCorrectAns]=useState(0);
+    const [showResult, setShowResult] = useState(false);
+    const [clicked, setClicked] = useState(false);
+    const hanldleAnswrOption=(isCorrect)=>{
+        if(isCorrect){
+            setScore(score+5)
+            setCorrectAns(CorrectAns+1)
+            toast.success('Correct Answer!',{theme:"colored"});
+        }
+        else{
+            setScore(score-1);
+            toast.error('Wrong Answer!',{theme:"colored"});
+        }
+        setClicked(true);
+    };
+    const handleNextOption=()=>{
+        setClicked(false) 
+        const nextQuestion = currentQuestion+1;
+        if(nextQuestion<questions.length){
+        setCurrentQuestion(nextQuestion);
+        }
+        else{
+            setShowResult(true)
+        }
+    };
+    const handlePlayAgain = () =>{
+        setCurrentQuestion(0)
+        setScore(0)
+        setCorrectAns(0)
+        setShowResult(false)
+    }   
+    return (
+        <div className='app'>
+            {showResult?(
+            <QuizResult score={score} CorrectAns={CorrectAns} handlePlayAgain = {handlePlayAgain}/>
+            ):(
+            <>
+            <div className='question-section'>
+                <h5>Score:{score}</h5>
+                <div className='question-count'>
+                    <span>Question {currentQuestion+1} of {questions.length}</span>
+                </div>
+                <div className='question-text'>
+                    {questions[currentQuestion].questionText}
+                </div>
+            </div>
+            <div className="answer-section">
+                {questions[currentQuestion].answerOptions.map((ans,i)=>{
+                    return (
+                    <button className={"button ${clicked && ans.isCorrect? 'correct':'button'}"}
+                    disabled={clicked}
+                    key={i} 
+                    onClick={()=>hanldleAnswrOption(ans.isCorrect)}
+                    >
+                        {ans.answerText}
+                        </button>
+                    );
+
+                })
+                }
+            <div className='actions'>
+                <button onClick={handlePlayAgain}>Quit</button>
+                <button disabled={!clicked} onClick={handleNextOption}>Next</button>
+            </div>
+            </div>
+            </>)}
+            <ToastContainer/>
+        </div>
+    );
+};
+export default Quizz
